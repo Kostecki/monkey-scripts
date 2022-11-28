35c1a0ac8983804598d1ce9b795a5361560600a2
Jacob Kostecki <jacob@kostecki.dk>
2022-11-29 00:26:05 +0100

    add sketchy timeout check to make it worth with search input

--- parcelsapp.js
+++ parcelsapp.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Add barcode with tracking number to results page for quick mobile access.
 // @author       You
-// @match        https://parcelsapp.com/en/tracking/*
+// @match        https://parcelsapp.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=parcelsapp.com
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/barcodes/JsBarcode.code128.min.js
@@ -12,16 +12,24 @@
 
 (function () {
   "use strict";
-
-  window.addEventListener("load", function () {
-    const trackingNumber = window.location.href.split("/tracking/")[1];
+  const insertBarcode = () => {
     const trackingInfoDOM = document.querySelector(".parcel-attributes");
 
-    trackingInfoDOM.insertAdjacentHTML(
-      "afterend",
-      '<div style="margin-top: -6px; margin-bottom: 15px; display: flex; justify-content: center;"><svg id="barcode"></svg></div>'
-    );
+    if (trackingInfoDOM) {
+      const trackingNumber = trackingInfoDOM.querySelector(
+        "tbody tr:first-of-type td:last-of-type"
+      ).innerText;
 
-    JsBarcode("#barcode", trackingNumber);
-  });
+      trackingInfoDOM.insertAdjacentHTML(
+        "afterend",
+        '<div style="margin-top: -6px; margin-bottom: 15px; display: flex; justify-content: center;"><svg id="barcode"></svg></div>'
+      );
+
+      JsBarcode("#barcode", trackingNumber);
+    } else {
+      setTimeout(() => insertBarcode(), 1000);
+    }
+  };
+
+  insertBarcode();
 })();
