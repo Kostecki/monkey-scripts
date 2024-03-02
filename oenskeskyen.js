055d1fc469ea4ae3600bb4c16a05ccea4fe46f56
Jacob Kostecki <jacob@kostecki.dk>
2024-03-03 00:54:46 +0100

    update ønskeskyen for violentmonkey

--- oenskeskyen.js
+++ oenskeskyen.js
@@ -1,71 +1,108 @@
 // ==UserScript==
-// @name         Ønskeskyen - Hvem gemmer sig?
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Hvem gemmer sig bag reservationerne
-// @author       You
-// @match        https://xn--nskeskyen-k8a.dk/share/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=xn--nskeskyen-k8a.dk
-// @grant        none
+// @name        Ønskeskyen.dk - Hvem giver?
+// @namespace   Violentmonkey Scripts
+// @match       https://onskeskyen.dk/*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=xn--nskeskyen-k8a.dk
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 3.3.2024 00.12.12
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // ==/UserScript==
 
-(function () {
-  "use strict";
+const maxTries = 20;
+const retryInterval = 250;
 
-  const users = [
-    {
-      id: 12344,
-      name: "Jacob K",
-    },
-    {
-      id: 27165,
-      name: "Helle K",
-    },
-    {
-      id: 66859,
-      name: "Jesper K",
-    },
-    {
-      id: 83607,
-      name: "Tonny K",
-    },
-  ];
+const wishContainerClassPartial = "GridDefaultWishes__Grid";
+const wishElementsClassPartial = "WishCard__WishCardContainer";
+const wishElementOverlayClassPartial = "Overlays__ReservedText-sc";
 
-  const listName = window.location.href.split("/share/")[1];
-  const url = `https://api.xn--nskeskyen-k8a.dk/api/share/${listName}`;
-  const userMap = [];
+const graphql = "https://api.gowish.com/graphql";
+const query = {
+  operationName: "getWishlistWishes",
+  variables: {
+    isLongQuery: true,
+    id: "STk4AY3MIk69Pq9y",
+    input: { cursor: 0, limit: 200 },
+    sort: { field: "index", direction: "ASC" },
+    filter: null,
+  },
+  query:
+    "query getWishlistWishes($id: ID!, $input: PaginationInput, $sort: WishSortingInput, $filter: SearchTerm, $isLongQuery: Boolean!) {\n  wishlist(id: $id) {\n    id\n    wishes(input: $input, sort: $sort, filter: $filter) {\n      data {\n        ...WishItemWithDirectives\n        __typename\n      }\n      totalCount\n      nextCursor\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment WishItemWithDirectives on Wish {\n  description\n  id\n  price\n  quantity\n  photos\n  index\n  title\n  url\n  currency\n  labels\n  redirectUrl\n  reservations @include(if: $isLongQuery) {\n    quantity\n    reservedBy {\n      id\n      firstName\n      lastName\n      profileImage\n      __typename\n    }\n    __typename\n  }\n  next @include(if: $isLongQuery) {\n    id\n    index\n    __typename\n  }\n  prev @include(if: $isLongQuery) {\n    id\n    index\n    __typename\n  }\n  productRef @include(if: $isLongQuery) {\n    id\n    uurl\n    countryCode\n    originalUrl\n    domainName\n    brand {\n      id\n      logo\n      __typename\n    }\n    __typename\n  }\n  __typename\n}",
+};
 
-  const updateDOM = () => {
-    const wishElements = document.querySelectorAll(".wish-list.sel li");
-    if (wishElements.length) {
-      userMap.forEach((e, i) => {
-        if (e) {
-          const elm = wishElements[i].querySelector(".reserve span");
+let savedUrl = "";
 
-          if (elm.innerHTML.includes("en anden")) {
-            elm.innerHTML = `Reserveret af\n${e}`;
+const fetchWishlist = async (url, payload) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  return response.json();
+};
+
+const addWishes = () => {
+  const id = window.location.href.split("wishlists/")[1];
+  const payload = { ...query, variables: { ...query.variables, id } };
+
+  fetchWishlist(graphql, payload).then((resp) => {
+    const wishes = resp.data.wishlist.wishes.data;
+    const wishElements = Array.from(
+      document.querySelectorAll(`[class^='${wishElementsClassPartial}']`)
+    );
+
+    wishes.forEach((wish, index) => {
+      if (wish.reservations.length > 0) {
+        const reservation = wish.reservations[0].reservedBy;
+        const name = `${reservation.firstName} ${reservation.lastName}`;
+        const newText = `<div style="text-align: center;">Reserveret af<br />${name.trim()}</div>`;
+
+        try {
+          wishElements[index].querySelector(
+            `[class^='${wishElementOverlayClassPartial}']`
+          ).innerHTML = newText;
+        } catch (err) {
+          console.error(wish, err);
+        }
+      }
+    });
+  });
+};
+
+const setup = () => {
+  const currentUrl = window.location.href;
+  if (currentUrl !== savedUrl) {
+    savedUrl = currentUrl;
+
+    let container = document.querySelector(
+      `[class^='${wishContainerClassPartial}']`
+    );
+    let tries = 0;
+
+    if (!container) {
+      const checkExists = setInterval(() => {
+        container = document.querySelector(
+          `[class^='${wishContainerClassPartial}']`
+        );
+
+        if (container || tries > maxTries) {
+          clearInterval(checkExists);
+
+          if (container) {
+            addWishes();
           }
         }
-      });
+
+        tries++;
+      }, retryInterval);
+    } else {
+      addWishes();
     }
-  };
-
-  if (listName && listName !== "lists") {
-    console.log("Ønskeskyen - Unhiding..");
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        data.wishes.forEach((wish, index) => {
-          if (wish.statusOwner) {
-            const user = users.find((user) => user.id === wish.statusOwner.id);
-            userMap.push(user?.name ?? wish.statusOwner.id);
-          } else {
-            userMap.push(false);
-          }
-        });
-
-        setTimeout(() => updateDOM(), 1000);
-      });
   }
-})();
+};
+
+VM.observe(document.body, () => setup());
