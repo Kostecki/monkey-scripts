52d3b1c4580e723002dfd4eef74234b6bc27b4e6
Jacob Kostecki <jacob@kostecki.dk>
2024-03-03 01:15:39 +0100

    update parcelsapp for violentmonkey

--- parcelsapp.js
+++ parcelsapp.js
@@ -1,35 +1,40 @@
 // ==UserScript==
-// @name         Parcels - Barcode
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Add barcode with tracking number to results page for quick mobile access.
-// @author       You
-// @match        https://parcelsapp.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=parcelsapp.com
-// @grant        none
-// @require      https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/barcodes/JsBarcode.code128.min.js
+// @name        Parcels - Barcode
+// @namespace   Violentmonkey Scripts
+// @match       https://parcelsapp.com/en/tracking/*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=parcelsapp.com
+// @grant       none
+// @version     1.0
+// @author      Jacob Kostecki
+// @description 3.3.2024 00.56.48
+// @require     https://cdn.jsdelivr.net/npm/jsbarcode@3.11.6/dist/barcodes/JsBarcode.code128.min.js
 // ==/UserScript==
 
-(function () {
-  "use strict";
-  const insertBarcode = () => {
-    const trackingInfoDOM = document.querySelector(".parcel-attributes");
+const setup = () => {
+  let trackingInfoTable = document.querySelector(".parcel-attributes");
 
-    if (trackingInfoDOM) {
-      const trackingNumber = trackingInfoDOM.querySelector(
-        "tbody tr:first-of-type td:last-of-type"
-      ).innerText;
+  if (!trackingInfoTable) {
+    const checkExists = setInterval(() => {
+      trackingInfoTable = document.querySelector(".parcel-attributes");
 
-      trackingInfoDOM.insertAdjacentHTML(
-        "afterend",
-        '<div style="margin-top: -6px; margin-bottom: 15px; display: flex; justify-content: center;"><svg id="barcode"></svg></div>'
-      );
+      if (trackingInfoTable) {
+        clearInterval(checkExists);
 
-      JsBarcode("#barcode", trackingNumber);
-    } else {
-      setTimeout(() => insertBarcode(), 1000);
-    }
-  };
+        if (trackingInfoTable) {
+          setup();
+        }
+      }
+    }, 100);
+  } else {
+    const trackingNumber = window.location.href.split("tracking/")[1];
 
-  insertBarcode();
-})();
+    trackingInfoTable.insertAdjacentHTML(
+      "afterend",
+      '<div style="margin-top: -6px; margin-bottom: 15px; display: flex; justify-content: center;"><svg id="barcode"></svg></div>'
+    );
+
+    JsBarcode("#barcode", trackingNumber);
+  }
+};
+
+setup();
